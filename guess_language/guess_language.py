cfcd4ca188bd2bc8f0eaeed9d0f611fc97dbfc8b
twCarllin <tw.carl.lin@gmail.com>
2019-10-23 06:14:48 +0000

    MOD: for python 3.6

--- guess_language/guess_language.py
+++ guess_language/guess_language.py
@@ -34,7 +34,7 @@ __all__ = 'guessLanguage guessLanguageName guessLanguageInfo guessLanguageTag gu
 
 import codecs, os, re, sys, unicodedata
 from collections import defaultdict
-from blocks import unicodeBlock
+from .blocks import unicodeBlock
 
 
 MIN_LENGTH = 20
@@ -292,8 +292,8 @@ def guessLanguage(text):
     if not text:
         return UNKNOWN
     
-    if isinstance(text, str):
-        text = unicode(text, 'utf-8')
+    # if isinstance(text, str):
+    #     text = str(text, 'utf-8')
     
     text = normalize(text)
     
@@ -451,7 +451,7 @@ def createOrderedModel(content):
     trigrams = defaultdict(int) # QHash<QString,int> 
     content = content.lower()
     
-    for i in xrange(0, len(content)-2):
+    for i in range(0, len(content)-2):
         trigrams[content[i:i+3]]+=1
 
     return sorted(trigrams.keys(), key=lambda k: (-trigrams[k], k))
@@ -476,7 +476,7 @@ def distance(model, knownModel):
 def _makeNonAlphaRe():
     nonAlpha = [u'[^']
     for i in range(sys.maxunicode):
-      c = unichr(i)
+      c = chr(i)
       if c.isalpha(): nonAlpha.append(c)
     nonAlpha.append(u']')
     nonAlpha = u"".join(nonAlpha)
